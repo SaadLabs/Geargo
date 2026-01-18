82e193bf0cda547b19aa3d2d5cabe377b66efee7
SaadLabs <s4saad2004@gmail.com>
2026-01-18 21:39:22 +0500

    Update kiya saad ne bari mehnet kar ke.

--- product page/product.js
+++ product page/product.js
@@ -1,3 +1,68 @@
+//  === Navbar Responsiveness ===
+
+
+const menuIcon = document.getElementById("menuIcon");
+const navContainer = document.getElementById("navContainer");
+
+menuIcon.addEventListener("click", () => {
+  navContainer.classList.toggle("active");
+  menuIcon.textContent = navContainer.classList.contains("active") ? "✕" : "☰";
+});
+
+// Function to handle Search Input Logic
+function setupSearchLogic(inputId, clearBtnId) {
+  const input = document.getElementById(inputId);
+  const clearBtn = document.getElementById(clearBtnId);
+
+  // Check if elements exist to prevent errors
+  if (!input || !clearBtn) return;
+
+  // Show/Hide "X" button when typing
+  input.addEventListener("input", () => {
+    if (input.value.length > 0) {
+      clearBtn.style.display = "block";
+    } else {
+      clearBtn.style.display = "none";
+    }
+  });
+
+  //  Clear text when "X" is clicked
+  clearBtn.addEventListener("click", () => {
+    input.value = "";              // Clear text
+    clearBtn.style.display = "none";
+    input.focus();
+  });
+}
+
+setupSearchLogic("searchInput", "clearBtn");
+setupSearchLogic("mobileSearchInput", "mobileClearBtn");
+
+// Listen for screen resize to reset mobile state on desktop
+const mobileSearchBarvis = document.querySelector('.mobile-search-bar');
+const breakpoint = 1100;
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > breakpoint) {
+    mobileSearchBarvis.style.display = 'none';
+  }
+});
+
+
+
+// Toggle mobile search bar
+const mobileSearchIcon = document.querySelector('.mobile-search-icon');
+const mobileSearchBar = document.querySelector('.mobile-search-bar');
+
+mobileSearchIcon.addEventListener('click', () => {
+  if (mobileSearchBar.style.display === 'block') {
+    mobileSearchBar.style.display = 'none';
+  } else {
+    mobileSearchBar.style.display = 'block';
+  }
+});
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
 
   // Reusable function for both Desktop and Mobile search
@@ -60,3 +125,5 @@ document.addEventListener("DOMContentLoaded", function () {
   // Initialize Mobile Search
   setupSearch('mobileSearchInput', 'mobileSearchResultsList');
 });
+
+
--- user profile/user.php
+++ user profile/user.php
@@ -50,6 +50,7 @@ $paymentMethods = getUserPaymentMethods($conn, $user_id);
 
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>GearGo | User Profile</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
