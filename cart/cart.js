2d71bd288d744ed1b5eba840c4d19b0227605dee
SaadLabs <s4saad2004@gmail.com>
2026-01-18 22:08:02 +0500

    Updated

--- cart/cart.js
+++ cart/cart.js
@@ -1,10 +1,12 @@
 function openCart() {
     document.getElementById("cart-sidebar").classList.add("active");
     document.getElementById("cart-overlay").classList.add("active");
+    document.body.classList.add('cart-open');
 }
 
 function closeCart() {
     document.getElementById("cart-sidebar").classList.remove("active");
     document.getElementById("cart-overlay").classList.remove("active");
+    document.body.classList.remove('cart-open');
 }
 
